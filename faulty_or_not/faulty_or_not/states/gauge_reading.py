9e50fabfae00f3207ffb1c4fbf0586793208c32d
mexuz <controleryan@gmail.com>
2026-02-24 16:35:07 -0300

    feat: implement circle flight functionality in Navigation state and enhance GaugeReading with stop_circle method

--- faulty_or_not/faulty_or_not/states/gauge_reading.py
+++ faulty_or_not/faulty_or_not/states/gauge_reading.py
@@ -39,7 +39,7 @@ class GaugeReading(State):
         if "inference_image_publisher" not in blackboard:
             blackboard["inference_image_publisher"] = self.node.create_publisher(CompressedImage, "/gauge/compressed", 10)
 
-        config = IMX219Config()
+        config = IMX219Config(brightness=-0.6)
 
         # config = OpenCVConfig(
         #     name="webcam",
@@ -161,6 +161,10 @@ class GaugeReading(State):
                     last_class_id = None
         finally:
             self.camera.close()
+            # Stop the circle flight immediately
+            nav = blackboard.get("navigation_state")
+            if nav is not None:
+                nav.stop_circle()
     
         
         if last_class_id is not None:            
