a4adb5d5736a8971353c809599a4f4c403875faa
Lucas Ronchi <lucascronchi2005@gmail.com>
2026-04-08 18:08:32 -0300

    fix: model in colcon build

--- bouncing/setup.py
+++ bouncing/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+
 
 package_name = 'bouncing'
 
@@ -7,9 +9,9 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ("share/" + package_name + "/models", glob("share/models/*")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
